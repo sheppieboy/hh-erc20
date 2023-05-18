6c4ed589d3f75ab5636cef2007b47740edde1502
Luke Sheppard <sheppiegit@gmail.com>
2023-05-18 19:33:07 -0400

    added helper config for hardhat to get the networks and initial supply.

---
+++ helper-hardhat.config.js
@@ -0,0 +1,15 @@
+const networkConfig = {
+  11155111: {
+    name: 'sepolia',
+  },
+  31337: {
+    name: 'hardhat',
+  },
+};
+
+const INITIAL_SUPPLY = '1000';
+
+module.exports = {
+  networkConfig,
+  INITIAL_SUPPLY,
+};
