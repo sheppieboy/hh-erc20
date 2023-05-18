2409f233cfd350de9f1c1552b50c0d3005c146b9
Luke Sheppard <sheppiegit@gmail.com>
2023-05-18 19:27:38 -0400

    Added the sepolia testnet and hardhat local host chains.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,20 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-// require('dotenv');
+require('dotenv');
 
-// const SEPOLIA_RPC_URL = process.env.PRIVATE_KEY;
-// const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const SEPOLIA_RPC_URL = process.env.PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: '0.8.18',
-  // defaultNetwork: 'hardhat',
-  // networks: {
-  //   hardhat: {
-  //     chainId: 31337,
-  //   },
-  //   sepolia: {
-  //     url: SEPOLIA_RPC_URL,
-  //     accounts: [PRIVATE_KEY],
-  //     chainId: 11155111,
-  //   },
-  // },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
 };
